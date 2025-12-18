2ceb5f9c0752a8e3db614154e92cc92fa149fcd2
Nicholas Pang <nicholaspangts@gmail.com>
2025-12-18 11:36:54 +0800

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <arr.h>
+#include <llist.h>
 using namespace std;
 
 int main() {
@@ -7,7 +9,31 @@ int main() {
 }
 
 void mainInterface(){
-    while (true){
-        // main content
+  int r;
+  while (true){
+    try
+    {
+      cout << "-----------------------------------------------------------------------" << endl 
+        << " Please Select Your Desired Data Structure Demo ( 1 or 2 or q to quit) " << endl
+        << "-----------------------------------------------------------------------" << endl
+        << " 1. Array" << endl
+        << " 2. Linked List" << endl;
+      cin >> r;
+      if (r == 1){
+        arrayInterface();
+      }
+      else if (r == 2){
+        linkedListInterface();
+      }
+      else{
+        throw r;
+      }
     }
+    catch(int r)
+    {
+      cout << "Invalid Input" << endl;
+      cin.clear();
+      cin.ignore(1000, '\n');
+    }  
+  }
 }
