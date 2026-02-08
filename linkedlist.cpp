1c334a61824226bfe3eefac6a6f1d0a93649cd99
Fraxtal <nicholaspangts@gmail.com>
2026-02-08 15:07:24 +0800

    Arranged structure

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,42 +1,55 @@
-//#pragma once
+// #pragma once
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <chrono>
-#include <windows.h> 
-#include <psapi.h> 
+#include <windows.h>
+#include <psapi.h>
 #include "linkedlist.hpp"
 using namespace std::chrono;
 using namespace std;
 
 LinkedList::LinkedList() = default;
-LinkedList::~LinkedList() {
-    Node* cur = head;
-    while (cur) { Node* next = cur->next; delete cur; cur = next; }
+LinkedList::~LinkedList()
+{
+	Node *cur = head;
+	while (cur)
+	{
+		Node *next = cur->next;
+		delete cur;
+		cur = next;
+	}
 }
 
-void LinkedList::insertion(int id, const string& name, int row, string col, const string& seatClass) {
-    Node* n = new Node{id, name, row, col, seatClass, nullptr};
-    if (!head) {
+void LinkedList::insertion(int id, const string &name, int row, string col, const string &seatClass)
+{
+	Node *n = new Node{id, name, row, col, seatClass, nullptr};
+	if (!head)
+	{
 		head = tail = n;
 	}
-    else { 
-		tail->next = n; tail = n; 
+	else
+	{
+		tail->next = n;
+		tail = n;
 	}
 }
 
-void LinkedList::load() {
+void LinkedList::load()
+{
 	auto start = high_resolution_clock::now();
 	ifstream file("data/flight_passenger_data.csv");
-	if (!file.is_open()) {
+	if (!file.is_open())
+	{
 		cout << "Error: Could not read file " << endl;
 		return;
 	}
 	string line;
 	string temp;
 	getline(file, line);
-	while(getline(file,line)) {
+	while (getline(file, line))
+	{
 		stringstream str2(line);
 		string segmt;
 		int id, seatRow;
@@ -54,25 +67,32 @@ void LinkedList::load() {
 	cout << "Loaded!" << endl;
 	std::cout << "Loading Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
 	PROCESS_MEMORY_COUNTERS pmc;
-    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
-        std::cout << "Memory Usage (Linked List): " 
-                  << pmc.WorkingSetSize / 1024 << " KB" << std::endl; // Convert to KB
-    } else {
-        std::cerr << "Unable to retrieve memory information." << std::endl;
-    }
+	if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc)))
+	{
+		std::cout << "Memory Usage (Linked List): "
+				  << pmc.WorkingSetSize / 1024 << " KB" << std::endl; // Convert to KB
+	}
+	else
+	{
+		std::cerr << "Unable to retrieve memory information." << std::endl;
+	}
 }
 
-bool LinkedList::deletion(int id) {
-	if (head->id == id) {
-		Node* toDel = head;
+bool LinkedList::deletion(int id)
+{
+	if (head->id == id)
+	{
+		Node *toDel = head;
 		head = head->next;
 		delete toDel;
 		return true;
 	}
-	Node* prev = head;
-	Node* cur = head->next;
-	while (cur != nullptr) {
-		if (cur->id == id) {
+	Node *prev = head;
+	Node *cur = head->next;
+	while (cur != nullptr)
+	{
+		if (cur->id == id)
+		{
 			prev->next = cur->next;
 			delete cur;
 			return true;
@@ -83,58 +103,91 @@ bool LinkedList::deletion(int id) {
 	return false;
 }
 
-Node* LinkedList::search(int id) {
-    for (Node* cur = head; cur; cur = cur->next) if (cur->id == id) return cur;
-    return nullptr;
+Node *LinkedList::search(int id)
+{
+	for (Node *cur = head; cur; cur = cur->next)
+		if (cur->id == id)
+			return cur;
+	return nullptr;
 }
 
-Node* LinkedList::split(Node *head) {
-	Node* full = head;
-	Node* half = head;
-	while (full != nullptr && full->next != nullptr) {
+Node *LinkedList::split(Node *head)
+{
+	Node *full = head;
+	Node *half = head;
+	while (full != nullptr && full->next != nullptr)
+	{
 		full = full->next->next;
-		if (full != nullptr) {
+		if (full != nullptr)
+		{
 			half = half->next;
 		}
 	}
-	Node* temp = half->next;
+	Node *temp = half->next;
 	half->next = nullptr;
 	return temp;
 }
 
-bool LinkedList::seatCompare(Node* first, Node* second) {
-	if (first->seatRow == second->seatRow) {
+bool LinkedList::seatCompare(Node *first, Node *second)
+{
+	if (first->seatRow == second->seatRow)
+	{
 		return (first->seatCol >= second->seatCol);
-	} 
+	}
 	return (first->seatRow > second->seatRow);
 }
 
-Node* LinkedList::merge(Node* first, Node* second, int criteria, bool desc) {
-	if (first == nullptr) {return second;}
-	if (second == nullptr) {return first;}
-	bool cmpResult = false;
-	switch (criteria) {
-		//ID
-		case 1: {cmpResult = (first->id>second->id);break;}
-		//name
-		case 2: {cmpResult = (first->name>=second->name);break;}
-		//seat
-		case 3: {cmpResult = seatCompare(first, second);break;}
+Node *LinkedList::merge(Node *first, Node *second, int criteria, bool desc)
+{
+	if (first == nullptr)
+	{
+		return second;
 	}
-	if ((cmpResult && desc) || (!cmpResult && !desc)) {
+	if (second == nullptr)
+	{
+		return first;
+	}
+	bool cmpResult = false;
+	switch (criteria)
+	{
+	// ID
+	case 1:
+	{
+		cmpResult = (first->id > second->id);
+		break;
+	}
+	// name
+	case 2:
+	{
+		cmpResult = (first->name >= second->name);
+		break;
+	}
+	// seat
+	case 3:
+	{
+		cmpResult = seatCompare(first, second);
+		break;
+	}
+	}
+	if ((cmpResult && desc) || (!cmpResult && !desc))
+	{
 		first->next = merge(first->next, second, criteria, desc);
 		return first;
-	} else {
+	}
+	else
+	{
 		second->next = merge(first, second->next, criteria, desc);
 		return second;
 	}
 }
 
-Node* LinkedList::mergeSort(Node* head, int criteria, bool desc) {
-	if (head == nullptr || head->next == nullptr) {
+Node *LinkedList::mergeSort(Node *head, int criteria, bool desc)
+{
+	if (head == nullptr || head->next == nullptr)
+	{
 		return head;
 	}
-	Node* second = split(head);
+	Node *second = split(head);
 
 	head = mergeSort(head, criteria, desc);
 	second = mergeSort(second, criteria, desc);
@@ -142,15 +195,18 @@ Node* LinkedList::mergeSort(Node* head, int criteria, bool desc) {
 	return merge(head, second, criteria, desc);
 }
 
-void LinkedList::sort(int criteria, bool desc) {
+void LinkedList::sort(int criteria, bool desc)
+{
 	auto start = high_resolution_clock::now();
 	head = mergeSort(head, criteria, desc);
-	if (head == nullptr) {
+	if (head == nullptr)
+	{
 		tail == nullptr;
 		return;
 	}
-	Node* cur = head;
-	while(cur->next != nullptr) {
+	Node *cur = head;
+	while (cur->next != nullptr)
+	{
 		cur = cur->next;
 	}
 	tail = cur;
@@ -158,119 +214,140 @@ void LinkedList::sort(int criteria, bool desc) {
 	std::cout << "Sorting Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
 }
 
-void LinkedList::display() const {
+void LinkedList::display() const
+{
 	auto start = high_resolution_clock::now();
-    for (Node* cur = head; cur; cur = cur->next)
-        cout << cur->id << ' ' << cur->name << ' ' << cur->seatRow << cur->seatCol << '\n';
+	for (Node *cur = head; cur; cur = cur->next)
+		cout << cur->id << ' ' << cur->name << ' ' << cur->seatRow << cur->seatCol << '\n';
 	auto end = high_resolution_clock::now();
 	std::cout << "Display Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
 }
 
-Node* LinkedList::getTail() {
+Node *LinkedList::getTail()
+{
 	return tail;
 }
 
-Node* LinkedList::getHead() {
+Node *LinkedList::getHead()
+{
 	return head;
 }
 
-void linkedListInterface(){
+void linkedListInterface()
+{
 	LinkedList list;
 	int option = -1;
 	bool running = true;
 	string temp;
 	while (running)
 	{
-		try {
+		try
+		{
 			option = -1;
-			cout << "Please select an operation to proceed(Enter option's number):" << endl 
-			<< "1. Load Passenger Data" << endl
-			<< "2. Insert New Data" << endl
-			<< "3. Delete Data" << endl
-			<< "4. Search by ID" << endl
-			<< "5. Sort Data" << endl
-			<< "6. Display Data" << endl
-			<< "7. Exit" << endl;
+			cout << "Please select an operation to proceed(Enter option's number):" << endl
+				 << "1. Load Passenger Data" << endl
+				 << "2. Insert New Data" << endl
+				 << "3. Delete Data" << endl
+				 << "4. Search by ID" << endl
+				 << "5. Sort Data" << endl
+				 << "6. Display Data" << endl
+				 << "7. Exit" << endl;
 			cin >> temp;
 			option = stoi(temp);
-			switch (option) {
-				case 1 : {
-					list.load();
-					break;
-				}
-				case 2 : {
-					string name, seatCol, seatClass;
-					int seatRow;
-					cout << "Please enter name, seat row, seat column and seat class: ";
-					cin >> name >> seatRow >> seatCol >> seatClass;
-					list.insertion((list.getTail()->id+1), name, seatRow, seatCol, seatClass);
-					break;
-				}
-				case 3 : {
-					int id;
-					cout << "Please enter the ID to delete: ";
-					cin >> id;
-					auto start = high_resolution_clock::now();
-					if (list.deletion(id)) {
-						cout << "Deletion completed!" << endl;
-					} else {
-						cout << "ID not found!" << endl;
-					}
-					auto end = high_resolution_clock::now();
-					std::cout << "Deletion Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
-					break;
-				}
-				case 4 : {
-					int id;
-					cout << "Please enter the ID to search: ";
-					cin >> id;
-					auto start = high_resolution_clock::now();
-					Node* temp = list.search(id);
-					if (temp != nullptr) {
-						cout << "ID: " << temp->id << endl
-						<< "Name: " << temp->name << endl
-						<< "Seat Row: " << temp->seatRow << endl
-						<< "Seat Column: " << temp->seatCol << endl
-						<< "Seat Class: " << temp->seatClass << endl;
-					} else {
-						cout << "ID not found!" << endl;
-					}
-					auto end = high_resolution_clock::now();
-					std::cout << "Deletion Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
-					break;
-				}
-				case 5 : {
-					int criteria;
-					string descending;
-					cout << "Please select the criteria to sort(Enter option's number):" << endl
-					<< "1. ID" << endl
-					<< "2. Name" << endl
-					<< "3. Seat" << endl;
-					cin >> criteria;
-					cout << "Do you want to sort it as descending order?(Enter \"Y\" for yes): ";
-					cin >> descending;
-					bool desc = (descending == "Y" ? true : false);
-					list.sort(criteria, desc);
-					cout << "Sort Done" << endl;
-					break;
-				}
-				case 6 : {
-					list.display();
-					cout << "The end of data"<< endl;
-					break;
-				}
-				case 7 : {
-					running = false;
-					break;
-				}
-				default : {
-					cout << "Invalid input! Please try again" << endl;
-					break;
-				}
+			switch (option)
+			{
+			case 1:
+			{
+				list.load();
+				break;
 			}
-		} catch (exception exception) {
+			case 2:
+			{
+				string name, seatCol, seatClass;
+				int seatRow;
+				cout << "Please enter name, seat row, seat column and seat class: ";
+				cin >> name >> seatRow >> seatCol >> seatClass;
+				list.insertion((list.getTail()->id + 1), name, seatRow, seatCol, seatClass);
+				break;
+			}
+			case 3:
+			{
+				int id;
+				cout << "Please enter the ID to delete: ";
+				cin >> id;
+				auto start = high_resolution_clock::now();
+				if (list.deletion(id))
+				{
+					cout << "Deletion completed!" << endl;
+				}
+				else
+				{
+					cout << "ID not found!" << endl;
+				}
+				auto end = high_resolution_clock::now();
+				std::cout << "Deletion Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
+				break;
+			}
+			case 4:
+			{
+				int id;
+				cout << "Please enter the ID to search: ";
+				cin >> id;
+				auto start = high_resolution_clock::now();
+				Node *temp = list.search(id);
+				if (temp != nullptr)
+				{
+					cout << "ID: " << temp->id << endl
+						 << "Name: " << temp->name << endl
+						 << "Seat Row: " << temp->seatRow << endl
+						 << "Seat Column: " << temp->seatCol << endl
+						 << "Seat Class: " << temp->seatClass << endl;
+				}
+				else
+				{
+					cout << "ID not found!" << endl;
+				}
+				auto end = high_resolution_clock::now();
+				std::cout << "Deletion Time: " << duration_cast<microseconds>(end - start).count() << " microseconds\n";
+				break;
+			}
+			case 5:
+			{
+				int criteria;
+				string descending;
+				cout << "Please select the criteria to sort(Enter option's number):" << endl
+					 << "1. ID" << endl
+					 << "2. Name" << endl
+					 << "3. Seat" << endl;
+				cin >> criteria;
+				cout << "Do you want to sort it as descending order?(Enter \"Y\" for yes): ";
+				cin >> descending;
+				bool desc = (descending == "Y" ? true : false);
+				list.sort(criteria, desc);
+				cout << "Sort Done" << endl;
+				break;
+			}
+			case 6:
+			{
+				list.display();
+				cout << "The end of data" << endl;
+				break;
+			}
+			case 7:
+			{
+				running = false;
+				break;
+			}
+			default:
+			{
+				cout << "Invalid input! Please try again" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception exception)
+		{
 			cout << "Invalid input! Please try again" << endl;
 		}
 	}
-
 }
