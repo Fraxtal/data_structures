4996dfe08932932b316e850e0fa622c342bce8af
02-is-02 <weihaowu89@gmail.com>
2026-02-09 08:47:44 +0800

    adjust for better indent, fixing typos

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -150,24 +150,24 @@ Node *LinkedList::merge(Node *first, Node *second, int criteria, bool desc)
 	bool cmpResult = false;
 	switch (criteria)
 	{
-	// ID
-	case 1:
-	{
-		cmpResult = (first->id > second->id);
-		break;
-	}
-	// name
-	case 2:
-	{
-		cmpResult = (first->name >= second->name);
-		break;
-	}
-	// seat
-	case 3:
-	{
-		cmpResult = seatCompare(first, second);
-		break;
-	}
+		// ID
+		case 1:
+		{
+			cmpResult = (first->id > second->id);
+			break;
+		}
+		// name
+		case 2:
+		{
+			cmpResult = (first->name >= second->name);
+			break;
+		}
+		// seat
+		case 3:
+		{
+			cmpResult = seatCompare(first, second);
+			break;
+		}
 	}
 	if ((cmpResult && desc) || (!cmpResult && !desc))
 	{
